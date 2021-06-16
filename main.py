bcfde23db52fd62fb318e4f9da2889acba5bddc0
Michael Groenewegen van der Weijden <michael@fragrant-chios.com>
2021-06-16 13:58:25 +0200

    finnished small rest api with flask

---
+++ main.py
@@ -0,0 +1,65 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource, reqparse, fields, marshal_with
+
+app = Flask(__name__)
+api = Api(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(100), nullable=False)
+    views=db.Column(db.Integer, nullable=False)
+    likes=db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name={name}, views={views}, likes={likes})"
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video is required", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes of the video is required", required=True)
+
+resource_fields = dict(
+    id=fields.Integer,
+    name=fields.String,
+    views=fields.Integer,
+    likes=fields.Integer
+)
+
+class Video(Resource):
+    @marshal_with(resource_fields)
+    def get(self, video_id):
+        result = VideoModel.query.filter_by(id=video_id).first()
+        return result
+
+    @marshal_with(resource_fields)
+    def post(self, video_id):
+        args = video_put_args.parse_args()
+        video = VideoModel(
+            id=video_id,
+            name=args['name'],
+            views=args['views'],
+            likes=args['likes']
+        )
+
+        db.session.add(video)
+        db.session.commit()
+
+        return video, 201
+    
+    # def put(self, videio_id):
+    #     pass 
+
+    # def delete(self, video_id):
+    #     abort_if_video_id_doesnt_extist(video_id)
+    #     del videos[video_id]
+    #     return '', 204
+
+api.add_resource(Video, "/video/<int:video_id>")
+
+if __name__ =="__main__":
+    app.run(debug=True)
+
